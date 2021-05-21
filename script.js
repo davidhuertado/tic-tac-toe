aa41ba57c1565b0205ea5ef5ad6cbf991b6273f7
davidhuertado <44754302+davidhuertado@users.noreply.github.com>
2021-05-21 20:38:44 -0300

    Added draw function. Refactoring.

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 'use strict';
-//TODO
-
-//boton de restart
 
 const playerFactory = (playerNumber, playerSymbol) => {
   return { playerNumber, playerSymbol };
@@ -27,24 +24,60 @@ const gameBoard = (() => {
 
 const player0 = playerFactory(0, 'x');
 const player1 = playerFactory(1, 'o');
-gameBoard.renderArray();
+
+//array of the squares in HTML
+const squares = document.querySelectorAll('.squares');
+
+//Event listener for every square
+squares.forEach((square) => {
+  square.addEventListener('click', function (e) {
+    const target = e.target;
+    const targetIdNumber = target.id.match(/\d+/);
+
+    // overwrite the board array
+
+    if (target.textContent == false && gameStateObject.gameState >= 0) {
+      if (gameStateObject.gameState % 2 === 0) {
+        gameBoard.arrayBoard[targetIdNumber] = 'x';
+        gameBoard.renderArray();
+        gameStateObject.checkWinner();
+        if (gameStateObject.gameState === 8) checkDraw();
+
+        if (gameStateObject.gameState !== -1) {
+          gameStateObject.gameState++;
+          gameStateObject.playerMessage.textContent = 'Player O turn';
+        }
+      } else {
+        // target.textContent = "o";
+        gameBoard.arrayBoard[targetIdNumber] = 'o';
+        gameBoard.renderArray();
+        gameStateObject.checkWinner();
+        if (gameStateObject.gameState === 8) checkDraw();
+        if (gameStateObject.gameState !== -1) {
+          gameStateObject.gameState++;
+          gameStateObject.playerMessage.textContent = 'Player X turn';
+        }
+      }
+    }
+  });
+});
 
 const gameStateObject = {
   gameState: 0,
   playerMessage: document.querySelector('.player-message'),
   displayWinner: function () {
-    if (gameStateObject.gameState === 0) {
+    if (gameStateObject.gameState % 2 === 0) {
       this.gameState = -1;
       this.playerMessage.textContent = `Player X wins`;
     } else {
       gameStateObject.gameState = -1;
-      this.playerMessage.textContent = `Player X wins`;
+      this.playerMessage.textContent = `Player O wins`;
     }
   },
 
   checkWinner: function () {
     let winnerSymbol;
-    if (this.gameState === 0) {
+    if (this.gameState % 2 === 0) {
       winnerSymbol = player0.playerSymbol;
     } else {
       winnerSymbol = player1.playerSymbol;
@@ -56,84 +89,99 @@ const gameStateObject = {
       gameBoard.arrayBoard[2] === winnerSymbol
     ) {
       this.displayWinner();
+      squares[0].style.backgroundColor = '#fc5185';
+      squares[1].style.backgroundColor = '#fc5185';
+      squares[2].style.backgroundColor = '#fc5185';
     } else if (
       gameBoard.arrayBoard[0] === winnerSymbol &&
       gameBoard.arrayBoard[3] === winnerSymbol &&
       gameBoard.arrayBoard[6] === winnerSymbol
     ) {
       this.displayWinner();
+      squares[0].style.backgroundColor = '#fc5185';
+      squares[3].style.backgroundColor = '#fc5185';
+      squares[6].style.backgroundColor = '#fc5185';
     } else if (
       gameBoard.arrayBoard[1] === winnerSymbol &&
       gameBoard.arrayBoard[4] === winnerSymbol &&
       gameBoard.arrayBoard[7] === winnerSymbol
     ) {
       this.displayWinner();
+      squares[1].style.backgroundColor = '#fc5185';
+      squares[4].style.backgroundColor = '#fc5185';
+      squares[7].style.backgroundColor = '#fc5185';
     } else if (
       gameBoard.arrayBoard[2] === winnerSymbol &&
       gameBoard.arrayBoard[5] === winnerSymbol &&
       gameBoard.arrayBoard[8] === winnerSymbol
     ) {
       this.displayWinner();
+      squares[2].style.backgroundColor = '#fc5185';
+      squares[5].style.backgroundColor = '#fc5185';
+      squares[8].style.backgroundColor = '#fc5185';
     } else if (
       gameBoard.arrayBoard[3] === winnerSymbol &&
       gameBoard.arrayBoard[4] === winnerSymbol &&
       gameBoard.arrayBoard[5] === winnerSymbol
     ) {
       this.displayWinner();
+      squares[3].style.backgroundColor = '#fc5185';
+      squares[4].style.backgroundColor = '#fc5185';
+      squares[5].style.backgroundColor = '#fc5185';
     } else if (
       gameBoard.arrayBoard[6] === winnerSymbol &&
       gameBoard.arrayBoard[7] === winnerSymbol &&
       gameBoard.arrayBoard[8] === winnerSymbol
     ) {
       this.displayWinner();
+      squares[6].style.backgroundColor = '#fc5185';
+      squares[7].style.backgroundColor = '#fc5185';
+      squares[8].style.backgroundColor = '#fc5185';
     } else if (
       gameBoard.arrayBoard[0] === winnerSymbol &&
       gameBoard.arrayBoard[4] === winnerSymbol &&
       gameBoard.arrayBoard[8] === winnerSymbol
     ) {
       this.displayWinner();
+      squares[0].style.backgroundColor = '#fc5185';
+      squares[4].style.backgroundColor = '#fc5185';
+      squares[8].style.backgroundColor = '#fc5185';
     } else if (
       gameBoard.arrayBoard[2] === winnerSymbol &&
       gameBoard.arrayBoard[4] === winnerSymbol &&
       gameBoard.arrayBoard[6] === winnerSymbol
     ) {
       this.displayWinner();
+      squares[2].style.backgroundColor = '#fc5185';
+      squares[4].style.backgroundColor = '#fc5185';
+      squares[6].style.backgroundColor = '#fc5185';
     }
   },
 };
 
-//array of the squares in HTML
-const squares = document.querySelectorAll('.squares');
-
-//Event listener for every square
-squares.forEach((square) => {
-  square.addEventListener('click', function (e) {
-    const target = e.target;
-    const targetIdNumber = target.id.match(/\d+/)[0];
-
-    // overwrite the board array
-
-    if (target.textContent == false && gameStateObject.gameState >= 0) {
-      if (gameStateObject.gameState === 0) {
-        gameBoard.arrayBoard[targetIdNumber] = 'x';
-        gameBoard.renderArray();
-        gameStateObject.checkWinner();
-        if (gameStateObject.gameState !== -1) {
-          gameStateObject.gameState++;
-          gameStateObject.playerMessage.textContent = 'Player O turn';
-        }
-      } else {
-        // target.textContent = "o";
-        gameBoard.arrayBoard[targetIdNumber] = 'o';
-        gameBoard.renderArray();
-        gameStateObject.checkWinner();
-        if (gameStateObject.gameState !== -1) {
-          gameStateObject.gameState--;
-          gameStateObject.playerMessage.textContent = 'Player X turn';
-        }
-      }
+const checkDraw = () => {
+  let emptySquare = false;
+  gameBoard.arrayBoard.forEach((square) => {
+    if (square == true) {
+      emptySquare = true;
+      return emptySquare;
     }
   });
-});
+  if (emptySquare == false) {
+    gameStateObject.playerMessage.textContent = 'Draw';
+    gameStateObject.gameState = -1;
+  }
+};
 
-document.querySelector('.restart-btn').addEventListener('click', () => {});
+//RESTART
+
+document.querySelector('.restart-btn').addEventListener('click', () => {
+  gameBoard.arrayBoard = ['', '', '', '', '', '', '', '', ''];
+  const squaresArray = [...squares];
+  squaresArray.forEach((square) => {
+    square.style.backgroundColor = '#fff';
+  });
+  gameBoard.renderArray();
+  gameStateObject.gameState = 0;
+  gameStateObject.playerMessage.textContent = 'Player X turn';
+});
